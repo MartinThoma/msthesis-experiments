2c85b218e74a53848809978619e79f4b25eeae7b
Martin Thoma <info@martin-thoma.de>
2017-04-05 12:57:14 +0200

    models/resnetpa.py: Fix

--- models/resnetpa.py
+++ models/resnetpa.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-"""
-Create a sequential model.
-
-Achieves 92% on cifar-10 according to https://github.com/raghakot/keras-resnet
-"""
+"""Create a resnet model."""
 
 from keras.models import Model
 from keras.layers import Input
@@ -14,8 +10,9 @@ from keras.layers import Convolution2D, AveragePooling2D
 from keras.regularizers import l2
 
 
-def rnpa_bottleneck_layer(input_tensor, nb_filters, filter_sz, stage,
-                          init='glorot_normal', reg=0.0, use_shortcuts=True):
+def residual_block(input_tensor, nb_filters, filter_sz, stage,
+                   kernel_initializer='he_uniform', l2reg=0.0,
+                   use_shortcuts=True):
     """Create a ResNet pre-activation bottleneck layer."""
     nb_in_filters, nb_bottleneck_filters = nb_filters
 
@@ -33,8 +30,8 @@ def rnpa_bottleneck_layer(input_tensor, nb_filters, filter_sz, stage,
         x = input_tensor
 
     x = Convolution2D(nb_bottleneck_filters, (1, 1),
-                      kernel_initializer=init,
-                      kernel_regularizer=l2(reg),
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=l2(l2reg),
                       use_bias=False,
                       name=conv_name + 'a')(x)
 
@@ -44,8 +41,8 @@ def rnpa_bottleneck_layer(input_tensor, nb_filters, filter_sz, stage,
     x = Activation('relu', name=relu_name + 'b')(x)
     x = Convolution2D(nb_bottleneck_filters, (filter_sz, filter_sz),
                       padding='same',
-                      kernel_initializer=init,
-                      kernel_regularizer=l2(reg),
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=l2(l2reg),
                       use_bias=False,
                       name=conv_name + 'b')(x)
 
@@ -54,8 +51,8 @@ def rnpa_bottleneck_layer(input_tensor, nb_filters, filter_sz, stage,
     x = BatchNormalization(axis=1, name=bn_name + 'c')(x)
     x = Activation('relu', name=relu_name + 'c')(x)
     x = Convolution2D(nb_in_filters, (1, 1),
-                      kernel_initializer=init,
-                      kernel_regularizer=l2(reg),
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=l2(l2reg),
                       name=conv_name + 'c')(x)
 
     # merge
@@ -69,9 +66,11 @@ def create_model(nb_classes=10, input_shape=(3, 32, 32),
                  layer1_params=(5, 64, 2),
                  res_layer_params=(3, 16, 3),
                  final_layer_params=None,
-                 init='glorot_normal', reg=0.0, use_shortcuts=True):
+                 kernel_initializer='he_uniform',
+                 l2reg=0.0,
+                 use_shortcuts=True):
     """
-    Create ResNet using full pre-activation.
+    Create ResNet using pre-activation.
 
     based on the work in
     "Identity Mappings in Deep Residual Networks"  by He et al
@@ -91,24 +90,20 @@ def create_model(nb_classes=10, input_shape=(3, 32, 32),
 
     Parameters
     ----------
-    input_dim : tuple of (C, H, W)
-
-    nb_classes:
-        number of scores to produce from final affine layer (input to softmax)
-
-    layer1_params: tuple of (filter size, num filters, stride for conv)
-
-    res_layer_params: tuple
-        of (filter size, num res layer filters, num res stages)
-
-    final_layer_params: None or tuple
-        of (filter size, num filters, stride for conv)
-
-    init: type of weight initialization to use
-
-    reg: L2 weight regularization (or weight decay)
-
-    use_shortcuts:
+    input_dim : tuple
+        (C, H, W) or (H, W, C), depending on the backend
+    nb_classes : int
+    layer1_params : tuple
+        (filter size, num filters, stride for conv)
+    res_layer_params : tuple
+        (filter size, num res layer filters, num res stages)
+    final_layer_params : None or tuple
+        (filter size, num filters, stride for conv)
+    kernel_initializer : string, optional (default: 'he_uniform')
+        type of kernel initialization to use
+    l2reg : float
+        L2 weight regularization (or weight decay)
+    use_shortcuts : bool
         to evaluate difference between residual and non-residual network
     """
     sz_L1_filters, nb_L1_filters, stride_L1 = layer1_params
@@ -121,43 +116,40 @@ def create_model(nb_classes=10, input_shape=(3, 32, 32),
     else:
         sz_pool_fin = input_shape[1] / (stride_L1)
 
-    img_input = Input(shape=input_shape, name='cifar')
+    img_input = Input(shape=input_shape, name='input')
 
     x = Convolution2D(nb_L1_filters, (sz_L1_filters, sz_L1_filters),
                       padding='same',
                       strides=(stride_L1, stride_L1),
-                      kernel_initializer=init,
-                      kernel_regularizer=l2(reg),
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=l2(l2reg),
                       use_bias=False,
                       name='conv0')(img_input)
     x = BatchNormalization(axis=1, name='bn0')(x)
     x = Activation('relu', name='relu0')(x)
 
     for stage in range(1, nb_res_stages + 1):
-        x = rnpa_bottleneck_layer(x,
-                                  (nb_L1_filters, nb_res_filters),
-                                  sz_res_filters,
-                                  stage,
-                                  init=init,
-                                  reg=reg,
-                                  use_shortcuts=use_shortcuts)
+        x = residual_block(x,
+                           (nb_L1_filters, nb_res_filters),
+                           sz_res_filters,
+                           stage,
+                           kernel_initializer=kernel_initializer,
+                           l2reg=l2reg,
+                           use_shortcuts=use_shortcuts)
 
     x = BatchNormalization(axis=1, name='bnF')(x)
     x = Activation('relu', name='reluF')(x)
-
     if use_final_conv:
         x = Convolution2D(nb_fin_filters, (sz_fin_filters, sz_fin_filters),
                           padding='same',
                           subsample=(stride_fin, stride_fin),
-                          kernel_initializer=init,
-                          kernel_regularizer=l2(reg),
+                          kernel_initializer=kernel_initializer,
+                          kernel_regularizer=l2(l2reg),
                           name='convF')(x)
 
     x = AveragePooling2D((sz_pool_fin, sz_pool_fin), name='avg_pool')(x)
-
     x = Flatten(name='flat')(x)
     x = Dense(nb_classes, activation='softmax', name='fc10')(x)
-
     return Model(img_input, x, name='rnpa')
 
 
@@ -165,5 +157,5 @@ if __name__ == '__main__':
     model = create_model(100, (32, 32, 3))
     model.summary()
     from keras.utils import plot_model
-    plot_model(model, to_file='seq3.png',
+    plot_model(model, to_file='resnetpa.png',
                show_layer_names=False, show_shapes=True)
