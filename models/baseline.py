27edce91fbfa2344dac957b98bbed8153f4a1ad5
Martin Thoma <info@martin-thoma.de>
2017-04-17 11:48:36 +0200

    models/baseline.py: Cleanup

--- models/baseline.py
+++ models/baseline.py
@@ -19,15 +19,28 @@ def create_model(nb_classes, input_shape, config=None):
     if config is None:
         config = {'model': {}}
 
-    # input_shape = (None, None, 3)  # for fcn
-    input_ = Input(shape=input_shape)
-    x = input_
     min_feature_map_dimension = min(input_shape[:2])
     if min_feature_map_dimension < 32:
         print("ERROR: Please upsample the feature maps to have at least "
               "a size of 32 x 32. Currently, it has {}".format(input_shape))
-    tmp = min_feature_map_dimension / 32.
     nb_filter = 32
+
+    if 'c1' in config['model']:
+        c1_nb_filter = config['model']['c1']['nb_filter']
+    else:
+        c1_nb_filter = nb_filter
+    if 'c8' in config['model']:
+        c8_nb_filter = config['model']['c8']['nb_filter']
+    else:
+        c8_nb_filter = 512
+
+    # Network definition
+    # input_shape = (None, None, 3)  # for fcn
+    input_ = Input(shape=input_shape)
+    x = input_
+
+    # Scale feature maps down to [63, 32] x [63, 32]
+    tmp = min_feature_map_dimension / 32.
     if tmp >= 2:
         while tmp >= 2.:
             for _ in range(2):
@@ -39,15 +52,6 @@ def create_model(nb_classes, input_shape, config=None):
             x = MaxPooling2D(pool_size=(2, 2))(x)
             # nb_filter *= 2
             tmp /= 2
-
-    if 'c1' in config['model']:
-        c1_nb_filter = config['model']['c1']['nb_filter']
-    else:
-        c1_nb_filter = nb_filter
-    if 'c8' in config['model']:
-        c8_nb_filter = config['model']['c8']['nb_filter']
-    else:
-        c8_nb_filter = 512
 
     x = Convolution2D(c1_nb_filter, (3, 3), padding='same',
                       kernel_initializer='he_uniform',
@@ -106,5 +110,5 @@ if __name__ == '__main__':
     model = create_model(100, (32, 32, 3))
     model.summary()
     from keras.utils import plot_model
-    plot_model(model, to_file='seq3.png',
+    plot_model(model, to_file='baseline.png',
                show_layer_names=False, show_shapes=True)
